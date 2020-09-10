41ba7d5f1f294619f5f28fbd8bfd94c2e14f7394
unknown <judithportocarrero99@gmail.com>
2020-09-10 04:36:36 -0500

    proyectos

---
+++ src/pages/contacto.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Cabecera from '../componets/header';
+
+const Contacto = () => {
+    return(
+        <div>
+            <Cabecera/>
+            <div>
+                <div>Contáctame</div>
+                <div>
+                    <p>Mi nombre:Judith Bertha Portocarrero Cachique</p>
+                    <p>https://www.linkedin.com/in/judith-portocarrero-cachique/</p>
+                    <p> 
+                        Portafolio
+                        https://app.talento.laboratoria.la/profile/Xn2oDrCJ3LX9ewXfXSd9wB6hLPI3
+                    </p>
+                    <p>
+                        Correos
+                        judith086.jpc@gmail.com
+                        judithportocarrero99@hotmail.com
+                    </p>
+                    <p>
+                        Teléfono
+                        979803191
+                        935744654
+                    </p>
+                    <p>
+                        Mi ubicación
+                        Lima, PERÚ   
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Contacto;
---
+++ src/pages/home.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Cabecera from '../componets/header';
+import './stylesPages/home.scss'
+import profile from '../images/perfil.png'
+import cv from '../images/cv.png'
+
+const Home = () => {
+    return(
+        <div className='containerHome'>
+            <Cabecera/>
+            <div className='containerDescriptionHome'>
+                <div className='titleHome'>Front end developer</div>
+                <div className='bodyHome'>
+                    <section className='containerImage'><img className='imgProfile' src={profile} alt=''foto de perfil/></section>
+                    <div className='containesDescription'>
+                        <section className='sobremi'>
+                            Desde muy pequeña tuve admiración por la tecnología para poder descubrir el porque 
+                            de las cosas, fue ahi donde me encontre con mundo lleno de posibilidades infinitas
+                            por explorar. En cada linea de código encontré mi pasión ya que puedo descubrir 
+                            una solución optima para cada problema usando las habilidades necesarias que adquirí
+                            a lo largo de mi carrera.
+                            <br/>
+                            Soy estudiante de VII ciclo la Universidad Nacional Federico Villareal 
+                            de la carrera de Ingeniería Informática y para complementar estos estudios
+                            me prepare en un curso intensivo como Front end developer en Laboratoria<br/><br/>
+                            <section><a className='cv' href='https://drive.google.com/file/d/1p1QXavMU56OyrnSNSEdfHlZUlIJwNliT/view?usp=sharing'><img className='logoCurriculum' src={cv} alt='logo curriculum'/>Ver Curriculum</a></section>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home;
