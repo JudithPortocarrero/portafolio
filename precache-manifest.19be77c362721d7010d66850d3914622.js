21588a8fbcbacdb16afa7e74d1de283608a7a41d
unknown <judithportocarrero99@gmail.com>
2020-09-10 05:09:43 -0500

    Updates

---
+++ precache-manifest.19be77c362721d7010d66850d3914622.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce02bb38304e5ac3b099dd464ff866d",
+    "url": "/portafolio/public/index.html"
+  },
+  {
+    "revision": "3b89f55e555e4cf4e50f",
+    "url": "/portafolio/public/static/css/main.b2d449ec.chunk.css"
+  },
+  {
+    "revision": "c77d0399c53bdeb5949a",
+    "url": "/portafolio/public/static/js/2.4827a9e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portafolio/public/static/js/2.4827a9e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b89f55e555e4cf4e50f",
+    "url": "/portafolio/public/static/js/main.6881438e.chunk.js"
+  },
+  {
+    "revision": "b42c86d93e140d834761",
+    "url": "/portafolio/public/static/js/runtime-main.fc3e5de7.js"
+  },
+  {
+    "revision": "25fb30cfd0717ee582f1c4a507b86732",
+    "url": "/portafolio/public/static/media/css.25fb30cf.png"
+  },
+  {
+    "revision": "d3568cdc173925d34d3ab0c5fa8acf76",
+    "url": "/portafolio/public/static/media/figma.d3568cdc.png"
+  },
+  {
+    "revision": "cbba441819dedfd0d32e7ba009faee5e",
+    "url": "/portafolio/public/static/media/firebase.cbba4418.png"
+  },
+  {
+    "revision": "53b52b2fb3c8b3a9ef7c4bf89b83c297",
+    "url": "/portafolio/public/static/media/git.53b52b2f.png"
+  },
+  {
+    "revision": "f2282f94c041c37ef4db028e428e41c1",
+    "url": "/portafolio/public/static/media/html.f2282f94.png"
+  },
+  {
+    "revision": "a5060ad166d91d7d838069144239e281",
+    "url": "/portafolio/public/static/media/javascript.a5060ad1.png"
+  },
+  {
+    "revision": "9ce68bcab87f651b5a6bd16489896ef7",
+    "url": "/portafolio/public/static/media/logoGit.9ce68bca.png"
+  },
+  {
+    "revision": "601ca06a1edbaef00d22cfcd3c63df24",
+    "url": "/portafolio/public/static/media/node.601ca06a.png"
+  },
+  {
+    "revision": "b7be43455daa87e763379a7ba17d383c",
+    "url": "/portafolio/public/static/media/perfil.b7be4345.png"
+  },
+  {
+    "revision": "9d6e78740402ac5dea5b897ca90e493a",
+    "url": "/portafolio/public/static/media/react.9d6e7874.png"
+  },
+  {
+    "revision": "6f3269eded20a2422c3bde250255efb3",
+    "url": "/portafolio/public/static/media/sass.6f3269ed.png"
+  },
+  {
+    "revision": "436c31c1fadb600d2a5bb43274120718",
+    "url": "/portafolio/public/static/media/web.436c31c1.png"
+  }
+]);
