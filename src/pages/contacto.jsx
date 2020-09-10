593889489a8b4a453e686666fa6074a1cbc30885
unknown <judithportocarrero99@gmail.com>
2020-09-10 05:18:19 -0500

    desplegando

--- src/pages/contacto.jsx
+++ src/pages/contacto.jsx
@@ -11,8 +11,8 @@ const Contacto = () => {
                 <div className='titleHome'>Contáctame</div>
                 <div className='divContacto'>
                     <p>Judith Bertha Portocarrero Cachique</p>
-                    <p><a className='liPage' href='https://www.linkedin.com/in/judith-portocarrero-cachique/'>linkedin<img className='linkedin' src={linkedin}/></a></p>
-                    <p><a className='liPage' href='https://app.talento.laboratoria.la/profile/Xn2oDrCJ3LX9ewXfXSd9wB6hLPI3'>Portafolio<img className='linkedin' src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/File-Explorer-fluent-icon.png/220px-File-Explorer-fluent-icon.png'/></a></p>
+                    <p><a className='liPage' href='https://www.linkedin.com/in/judith-portocarrero-cachique/'>linkedin<img className='linkedin' src={linkedin} alt='link Linkedin'/></a></p>
+                    <p><a className='liPage' href='https://app.talento.laboratoria.la/profile/Xn2oDrCJ3LX9ewXfXSd9wB6hLPI3'>Portafolio<img className='linkedin' src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/File-Explorer-fluent-icon.png/220px-File-Explorer-fluent-icon.png' alt='link Portafolio'/></a></p>
                     <p>
                         Correos:<br/>
                         judith086.jpc@gmail.com<br/>
